15b7b240a78ef0062d5fd10ce51b1230d2a10458
arifefa <arifefa@gradin.co.id>
2020-09-16 16:04:08 +0700

    CHANGE BREADCRUMB HOME TO PRODUCT

--- plugin/woocommerce/functions.php
+++ plugin/woocommerce/functions.php
@@ -10,3 +10,21 @@ function woo_rename_tabs($tabs)
     $tabs['additional information']['title'] = __('SPESIFIKASI');
     return $tabs;
 }
+
+
+/*==========================================================
+// CHANGE BREADCRUMB HOME TO PRODUCT  
+==========================================================*/
+add_filter('woocommerce_breadcrumb_home_url', 'woo_custom_breadrumb_home_url');
+function woo_custom_breadrumb_home_url()
+{
+    return '/products/';
+}
+
+add_filter('woocommerce_breadcrumb_defaults', 'wcc_change_breadcrumb_home_text', 20);
+function wcc_change_breadcrumb_home_text($defaults)
+{
+    // Change the breadcrumb home text from 'Home' to 'Products'
+    $defaults['home'] = 'Products';
+    return $defaults;
+}
