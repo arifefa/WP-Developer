feb9aca705bc75a8283050135610e5fc1759aa59
arifefa <arifefa@gradin.co.id>
2020-09-21 10:08:49 +0700

    set chaty cara ke 2

--- plugin/chaty/custom.js
+++ plugin/chaty/custom.js
@@ -1,7 +1,12 @@
+
 <script>
-     jQuery(document).ready(function(){
-  jQuery(".chaty-widget-i").click(function(){
-    jQuery("#chaty-channel-custom_link a").attr("href", "https://api.whatsapp.com/");
-  });
-});
+	jQuery(document).ready(function(){
+		jQuery(".chaty-widget-i").click(function () {
+			jQuery("#chaty-channel-custom_link a").attr("href", "https://api.whatsapp.com/");
+		});
+	});
+
+	jQuery(document).ready(function(){
+		jQuery("[href='https://web.whatsapp.com/send?phone=6281234567']").attr("href", "https://api.whatsapp.com/send?phone=6281234567&text=Hi%2C%");
+	});
 </script>
