d1f425cb61a5edc92c2fe86a84097102e811e875
arifefa <arifefa@gradin.co.id>
2020-11-04 13:19:47 +0700

    problem solving 2 colom cf7 + custom.js = placeholder, add val by attribut name

--- custom.js
+++ custom.js
@@ -1,12 +1,12 @@
 <script>
-    // CHANGE TAG HTML
+	// CHANGE TAG HTML
         $('.thisclass').replaceWith('<p class="thisclass">' + $('.thisclass').html() +'</p>')
 
 	// CHANGE TAG HTML VERSI 2
 		var attrs = {};
 
 		$.each($(".home .exp-post-title")[0].attributes, function(idx, attr) {
-        attrs[attr.nodeName] = attr.nodeValue;
+		attrs[attr.nodeName] = attr.nodeValue;
 		});
 
 		$(".home .exp-post-title").replaceWith(function () {
@@ -15,7 +15,7 @@
 
     // CHANGE ATRIBUTE AHREF
         $(document).ready(function(){
-        $("[href='https://google.com']").attr("href", "https://google.com/new");
+		$("[href='https://google.com']").attr("href", "https://google.com/new");
         });
 
     // CHANGE ATRIBUTE BY ID
@@ -26,5 +26,14 @@
 
 	// ALT text to images that does not have alt attribute:
 		$('#map_canvas > img:not([alt])').attr('alt', 'Alternative text');
+
+	// ADD VALUE BY ATTRIBUT NAME
+	    $( "[name='nama']" ).val("Hello bro");
+
+	// PLACEHOLDER
+		placeholder="Name :&#10;Kota :&#10;Email :&#10;Komontar :"
+		jquery("#comment").attr("placeholder", "Name :&#10;Kota :&#10;Email :&#10;Komontar :");
+		jQuery("#comment").attr("placeholder", " Name : \n Kota : \n Email : \n KomEntar :");
+		jQuery('#comment').val(' Name : \n Kota : \n Email : \n Komentar :');
 
 </script>
