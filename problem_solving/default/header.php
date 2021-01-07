90413af8c39d49b057aada7a2121175b339d0267
arifefa <arifefa@gradin.co.id>
2021-01-07 15:08:13 +0700

    how to fix spam boot

--- problem_solving/default/header.php
+++ problem_solving/default/header.php
@@ -5,6 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Current Theme </title>
+    <!-- iniCustom -->
+    <link rel="canonical" href="<?php echo get_site_url() ?>">
+    <link rel="canonical" href="<?php echo get_link_url() ?>">
+
+
+    <!-- iniCustom -->
+
 
 </head>
 
