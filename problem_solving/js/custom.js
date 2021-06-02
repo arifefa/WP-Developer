7dcd088563bc51180e91965f4720c6f9967fd4a5
arifefa <arifefa@gradin.co.id>
2021-06-02 17:36:52 +0700

    set auto update qty cart & spesific footer

--- problem_solving/js/custom.js
+++ problem_solving/js/custom.js
@@ -1,39 +1,71 @@
-<script>
-	// CHANGE TAG HTML
-        $('.thisclass').replaceWith('<p class="thisclass">' + $('.thisclass').html() +'</p>')
 
-	// CHANGE TAG HTML VERSI 2
-		var attrs = {};
+// CHANGE TAG HTML
+$('.thisclass').replaceWith('<p class="thisclass">' + $('.thisclass').html() + '</p>')
 
-		$.each($(".home .exp-post-title")[0].attributes, function(idx, attr) {
-		attrs[attr.nodeName] = attr.nodeValue;
-		});
+// CHANGE TAG HTML VERSI 2
+var attrs = {};
 
-		$(".home .exp-post-title").replaceWith(function () {
-			return $("<p />", attrs).append($(this).contents());
-		});
+$.each($(".home .exp-post-title")[0].attributes, function (idx, attr) {
+	attrs[attr.nodeName] = attr.nodeValue;
+});
 
-    // CHANGE ATRIBUTE AHREF
-        $(document).ready(function(){
-		$("[href='https://google.com']").attr("href", "https://google.com/new");
-        });
+$(".home .exp-post-title").replaceWith(function () {
+	return $("<p />", attrs).append($(this).contents());
+});
 
-    // CHANGE ATRIBUTE BY ID
-        $("#iniID a").attr("href", "https://google.com");
+// CHANGE ATRIBUTE AHREF
+$(document).ready(function () {
+	$("[href='https://google.com']").attr("href", "https://google.com/new");
+});
 
-	// ALT TO ALL IMG:
-		$('#map_canvas > img').attr('alt', 'Alternative text');
+// CHANGE ATRIBUTE BY ID
+$("#iniID a").attr("href", "https://google.com");
 
-	// ALT text to images that does not have alt attribute:
-		$('#map_canvas > img:not([alt])').attr('alt', 'Alternative text');
+// ALT TO ALL IMG:
+$('#map_canvas > img').attr('alt', 'Alternative text');
 
-	// ADD VALUE BY ATTRIBUT NAME
-	    $( "[name='nama']" ).val("Hello bro");
+// ALT text to images that does not have alt attribute:
+$('#map_canvas > img:not([alt])').attr('alt', 'Alternative text');
 
-	// PLACEHOLDER
-		placeholder="Name :&#10;Kota :&#10;Email :&#10;Komontar :"
-		jquery("#comment").attr("placeholder", "Name :&#10;Kota :&#10;Email :&#10;Komontar :");
-		jQuery("#comment").attr("placeholder", " Name : \n Kota : \n Email : \n KomEntar :");
-		jQuery('#comment').val(' Name : \n Kota : \n Email : \n Komentar :');
+// ADD VALUE BY ATTRIBUT NAME
+$("[name='nama']").val("Hello bro");
 
-</script>
+// PLACEHOLDER
+placeholder = "Name :&#10;Kota :&#10;Email :&#10;Komontar :"
+jquery("#comment").attr("placeholder", "Name :&#10;Kota :&#10;Email :&#10;Komontar :");
+jQuery("#comment").attr("placeholder", " Name : \n Kota : \n Email : \n KomEntar :");
+jQuery('#comment').val(' Name : \n Kota : \n Email : \n Komentar :');
+
+// BREADCRUMB YOAST
+add_shortcode('display_custom_breadcrumb', 'custom_breadcrumb');
+function custom_breadcrumb() {
+	if (function_exists('yoast_breadcrumb')) {
+		yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
+	}
+}
+
+breadcrumb woocomerce
+add_shortcode('display_custom_breadcrumb', 'woocommerce_breadcrumb');
+
+
+//SPESIFIC FOOTER
+if (jQuery(".pageAbout").length || jQuery(".pageContact").length || jQuery(".pageFaq").length) {
+	jQuery(".footerGlobal").css("display", "none");
+	jQuery(".footerHome").css("display", "block");
+}
+
+//WOOCOMEERCE - auto update quantity cart
+var timeout;
+jQuery(function ($) {
+	$('.woocommerce').on('change', 'input.qty', function () {
+		if (timeout !== undefined) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(function () {
+			$("[name='update_cart']").trigger("click");
+		}, 1000);
+	});
+});
+
+
+
